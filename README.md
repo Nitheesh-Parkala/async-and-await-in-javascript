30bbcd3215c5d7c28ec97833bb535d1848dc8900
Nitheesh26 <nitheeshprabhu2001@gmail.com>
2023-12-08 18:32:35 +0530

    async and await intro

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # async-and-await-in-javascript
+
+## what is async?
+-A async is a keyword that is used before a function to create a async function.
+-It Should always return a promise.
+
+## Using async and await.
+- It is a combo used to handle the promise.
+-A await is a keyword that is used only inside the async function.
---
+++ javascript/index.js
@@ -0,0 +1,56 @@
+// async function getData(){
+//     return "HI";
+// }
+// const dataPromise= getData();
+// console.log(dataPromise);
+
+// dataPromise.then((res)=>console.log(res));
+
+//Example 2;
+
+// const p = new Promise((resolve,reject)=>{
+//     resolve("HI Nitheesh Prabhu");
+// })
+
+// async function getData(){
+//     return p;
+// }
+
+// const dataPromise = getData();
+// console.log(dataPromise);
+// dataPromise.then((res)=>console.log(res));
+
+// using Async and await.........................................................................................................
+// It is a normal promise function.............................................................
+// const p = new Promise((resolve,reject)=>{
+//     resolve("HI")
+// });
+
+// function getData(){
+//     p.then((res)=>console.log(res))
+// }
+// getData();
+
+// //using Async and await.................................................................
+// const n = new Promise ((resolve,reject)=>{
+//     resolve("using await")
+// });
+
+// async function handlePromise(){
+//     const val = await n;
+//     console.log(val)
+// }
+// handlePromise();
+
+//using Async and await with setTimeout;...............................................................................
+const k = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("using set time out function to see how it will work.")
+    },10000)
+})
+
+async function handlePromise(){
+    const val = await k;
+    console.log(val)
+}
+handlePromise();
